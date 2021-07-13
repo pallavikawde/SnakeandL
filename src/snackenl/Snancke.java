a81bbdccc41632588f67abef9c5ecbddf75ba8ae
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-13 12:21:04 +0530

    Added:uc1

--- src/snackenl/Snancke.java
+++ src/snackenl/Snancke.java
@@ -4,4 +4,14 @@ public class Snancke {
 	public static void main (String[] args) {
 	
 		 System.out.println("welcome to Snack&adder");
-	}}
+		 
+		 int position=0;
+		 System.out.println("Start From -" +position);
+		 
+		 
+		 
+		 
+		 
+	}
+}
+			
