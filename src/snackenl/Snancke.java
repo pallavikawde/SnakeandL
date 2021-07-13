e61e12c11a408d376ce577a9b058730fd14e23c2
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-13 13:34:38 +0530

    Added:UC3

--- src/snackenl/Snancke.java
+++ src/snackenl/Snancke.java
@@ -3,8 +3,17 @@ package snackenl;
 
 
 public class Snancke {
-	public static void main (String[] args) {
 	
+	
+	static final int noPlay = 0;
+	static final int ladder=1;
+	static final int snake=2;
+
+	
+	public static void main (String[] args) {
+		
+		
+		
 		 System.out.println("welcome to Snack&adder");
 		 
 		 int position=0;
@@ -13,8 +22,26 @@ public class Snancke {
 		int die=(int)Math.floor(Math.random() *10) %6+1;
 		System.out.println("die is -" +die);
 		 
-		 
-		 
+		int option=(int)Math.floor(Math.random() *10) %3;
+		
+		
+		  
+		switch(option) {
+		case noPlay:
+			System.out.println("player stay in same position = " +position );
+		
+			break;
+		case ladder:
+			position=position+die;
+			System.out.println("player moves ahead = "+position+" die = "+die);
+			break;
+		case snake:
+			position =position-die;
+			System.out.println("player moves behind = " +position+" die = "+die);
+			break;
+			default:
+				break;
 	}
+		}
 }
 			
