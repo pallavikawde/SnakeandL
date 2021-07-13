59c8e40031fe685943fa14da9ab919f15dd5ad3a
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-13 20:04:39 +0530

    Added:UC7

--- src/snackenl/Snancke.java
+++ src/snackenl/Snancke.java
@@ -14,20 +14,24 @@ public class Snancke {
 		
 		int count=0;
 		int i=0;
-		
+		int p=1;
 		 System.out.println("welcome to Snack&adder");
 		 
-		 int position=0;
+		 int position1=0;
+		 int position2=0;
 		 
 		 
-		 System.out.println("Start From -" +position);
+		 
+		 System.out.println("Start From -" +position1);
 		 
 		int die;
 		
 		 
 		int option;
 		
-			while(position<=100 && i==0) {
+			while(i==0) {
+		
+			while(position1<=100 && i==0 && p==1) {
 				
 			
 			die=(int)Math.floor(Math.random() *10) %6+1;
@@ -40,31 +44,83 @@ public class Snancke {
 		  
 		switch(option) {
 		case noPlay:
-			System.out.println("player stay in same position = " +position );
-		
+			System.out.println("player stay in same position = " +position1 );
+			p=2;
+			
 			break;
 		case ladder:
-			position=position+die;
-			System.out.println("player moves ahead = "+position+" die = "+die);
+			position1=position1+die;
+			System.out.println("player moves ahead = "+position1+" die = "+die);
 			
-				if (position>=100) {
+				if (position1>=100) {
 				
-				if (position!=100) {
-				position=position-die;
+				if (position1!=100) {
+				position1=position1-die;
 				}
 				else {
-					System.out.println("winning position = "+ position);
+					System.out.println(" PLAYER1 WON  ****** POSITION = "+ position1);
 					i=1;
 				}
 				}
 			break;
 		case snake:
-			position =position-die;
-			if(position<0) {
-				position=0;
+			position1 =position1-die;
+			if(position1<0) {
+				position1=0;
+				p=2;
 				
 			}
-			System.out.println("player moves behind = " +position+" die = "+die);
+			System.out.println("player moves behind = " +position1+" die = "+die);
+			break;
+			default:
+				break;
+		}
+		
+		
+	
+			}
+			
+
+			while(position2<=100 && i==0 && p==2) {
+				
+			
+			die=(int)Math.floor(Math.random() *10) %6+1;
+			System.out.println("die is -" +die);
+			
+			count ++;	
+			
+			
+			option=(int)Math.floor(Math.random() *10) %3;
+		  
+		switch(option) {
+		case noPlay:
+			System.out.println("player stay in same position = " +position2 );
+			p=1;
+			
+			break;
+		case ladder:
+			position2=position2+die;
+			System.out.println("player moves ahead = "+position2+" die = "+die);
+			
+				if (position2>=100) {
+				
+				if (position2!=100) {
+				position2=position2-die;
+				}
+				else {
+					System.out.println("PLAYER2 WON ***** position = "+ position2);
+					i=1;
+				}
+				}
+			break;
+		case snake:
+			position2 =position2-die;
+			if(position2<0) {
+				position2=0;
+				p=1;
+				
+			}
+			System.out.println("player moves behind = " +position2+" die = "+die);
 			break;
 			default:
 				break;
@@ -75,6 +131,13 @@ public class Snancke {
 			}
 			
 			 
+
+			
+			 
+			
+			
+			
+			}
 			
 				System.out.println("dice rolled = " + count+" times");
 		}
