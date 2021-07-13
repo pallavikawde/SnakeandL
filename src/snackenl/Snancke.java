f8d51a76111c3c062c6218de97494e81923a14cc
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-13 12:44:39 +0530

    Added:UC2

--- src/snackenl/Snancke.java
+++ src/snackenl/Snancke.java
@@ -1,5 +1,7 @@
 package snackenl;
 
+
+
 public class Snancke {
 	public static void main (String[] args) {
 	
@@ -8,7 +10,8 @@ public class Snancke {
 		 int position=0;
 		 System.out.println("Start From -" +position);
 		 
-		 
+		int die=(int)Math.floor(Math.random() *10) %6+1;
+		System.out.println("die is -" +die);
 		 
 		 
 		 
